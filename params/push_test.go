80aba92e0f692de2f0dc03672d5c083743c625dd
yangfeng <smartwalle@gmail.com>
2016-02-19 10:43:53 +0800

    update

--- params/push_test.go
+++ params/push_test.go
@@ -1,36 +1,36 @@
 package params_test
 
-import (
-	"fmt"
-	"testing"
-	"github.com/smartwalle/taobao/params"
-	"github.com/smartwalle/taobao"
-)
+//import (
+//	"fmt"
+//	"testing"
+//	"github.com/smartwalle/taobao/params"
+//	"github.com/smartwalle/taobao"
+//)
 
-func TestCloudPushNoticeToIOSParam(t *testing.T) {
-	var p = params.CloudPushNoticeToIOSParam{}
-	p.Summary = "golang"
-	p.Target = "all"
-	p.Environment = "DEV"
-	p.TargetValue = "e"
-	p.AddParam("badge", 3)
-	p.AddParam("sound", "v")
-	p.AddParam("aaa", "eee")
-
-	fmt.Println("===== TestCloudPushNoticeToIOSParam =====")
-	fmt.Println(taobao.RequestWithKey("23201003", "1e2dfd16981d75142597fd10131b17b5", p))
-}
-
-func TestCloudPushNoticeToAndroidParam(t *testing.T) {
-	var p = params.CloudPushNoticeToAndroidParam{}
-	p.Title = "ttt"
-	p.Summary = "aaaa"
-	p.Target = "all"
-	p.TargetValue = "e"
-
-	fmt.Println("===== TestCloudPushNoticeToAndroidParam =====")
-	fmt.Println(taobao.RequestWithKey("23201003", "1e2dfd16981d75142597fd10131b17b5", p))
-}
+//func TestCloudPushNoticeToIOSParam(t *testing.T) {
+//	var p = params.CloudPushNoticeToIOSParam{}
+//	p.Summary = "golang"
+//	p.Target = "all"
+//	p.Environment = "DEV"
+//	p.TargetValue = "e"
+//	p.AddParam("badge", 3)
+//	p.AddParam("sound", "v")
+//	p.AddParam("aaa", "eee")
+//
+//	fmt.Println("===== TestCloudPushNoticeToIOSParam =====")
+//	fmt.Println(taobao.RequestWithKey("23201003", "1e2dfd16981d75142597fd10131b17b5", p))
+//}
+//
+//func TestCloudPushNoticeToAndroidParam(t *testing.T) {
+//	var p = params.CloudPushNoticeToAndroidParam{}
+//	p.Title = "ttt"
+//	p.Summary = "aaaa"
+//	p.Target = "all"
+//	p.TargetValue = "e"
+//
+//	fmt.Println("===== TestCloudPushNoticeToAndroidParam =====")
+//	fmt.Println(taobao.RequestWithKey("23201003", "1e2dfd16981d75142597fd10131b17b5", p))
+//}
 
 //func TestCloudPushMessageToIOSParam(t *testing.T) {
 //	var p = params.CloudPushMessageToIOSParam{}
